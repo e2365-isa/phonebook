6fe85baadddac146ed089e0303732e92c30ff4b8
isa <isa.aydin.2335@gmail.com>
2024-10-24 23:54:32 +0300

    last version

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "e2365-isa"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "i"
+}
+
+variable "hosted-zone" {
+  default = "isaaydindevops.com"
+}
